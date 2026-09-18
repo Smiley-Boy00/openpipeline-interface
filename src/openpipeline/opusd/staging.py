a9ff98210d20f6e484b52006cb758a4238735b85
David Martinez <david.e.margon@hotmail.com>
2026-09-18 13:46:47 -0700

    Update usd staging module

--- src/openpipeline/opusd/staging.py
+++ src/openpipeline/opusd/staging.py
@@ -1,13 +1,28 @@
 import subprocess
+
 from pxr import Usd
 
-def create_new_stage(file_path:str):
+
+def create_or_open_stage(stage_path:str, create:bool=False):
     ''' 
     File path must include usd file extension.
-    Returns a USD stage created from the provided file path.
+    Returns a USD stage from the provided file path.
     '''
-    stage: Usd.Stage = Usd.Stage.CreateNew(file_path)
+    if create:
+        stage: Usd.Stage = Usd.Stage.CreateNew(stage_path)
+    else:
+        stage: Usd.Stage = Usd.Stage.Open(stage_path)
     return stage
 
-def run_usdview_stage(file_path:str):
-    subprocess.run(['usdview', file_path], stderr=subprocess.PIPE)
+def run_usdview_stage(stage_path:str):
+    subprocess.Popen(['usdview', stage_path], stderr=subprocess.PIPE)
+
+def get_property_names(prim: Usd.Prim, attrOnly=False):
+    if attrOnly:
+        for attr in prim.GetAttributes():
+                str_split = str(attr).split(".")[-1].split("GetAttribute")[-1]
+                attr_name = str_split.strip("(')")
+                print(attr_name)
+    else:
+        for prop_name in prim.GetPropertyNames():
+            print(prop_name)
